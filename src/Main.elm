e62bd452b38674d84fdd9fe284cc28ac2d670ba2
gumelarme <gumelar.pn@gmail.com>
2023-12-24 03:05:12 +0800

    Working get sample text with generated Env.elm file

--- src/Main.elm
+++ src/Main.elm
@@ -1,36 +1,109 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, text)
+import List
+import Html exposing (Html, div, text, select, option)
 import Html.Attributes exposing (class)
+import Html.Attributes exposing (hidden)
+import Html exposing (button)
+import Http
+import Json.Decode exposing (Decoder, map, map3, field, string, list)
+import Env
 
 
-main: Program () Model Msg
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
-type alias Model = String
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
-init : Model
-init = "Hello"
+type  Model
+    = Loading
+    | Success (Data SampleText)
 
-type Msg = NoOp
 
 
-update : Msg -> Model -> Model
-update _ model = model
+init : ()  -> (Model, Cmd Msg)
+init _ =
+    (Loading, getSampleText)
+
+
+type Msg =
+    GotData (Result Http.Error (Data SampleText))
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        GotData result ->
+            case result of
+                Ok data ->
+                    (Success data, Cmd.none)
+                Err _ ->
+                    (Loading, Cmd.none)
+
 
 
 view : Model -> Html Msg
-view _ =
+view model =
   div []
-      [viewHeader]
+      [viewHeader model]
 
 
 
-viewHeader : Html Msg
-viewHeader =
-    div [class "flex p-2 bg-blue-800 text-white text-3xl font-bold"]
-        [text "Hanzi Memo"]
+viewHeader : Model -> Html Msg
+viewHeader model =
+    div [ class "flex p-2 px-4 bg-blue-800 text-white items-center" ]
+        [ div [ class "grow text-2xl font-bold"] [text "Hanzi Memo"]
+        , div [ class "flex items-center gap-5"]
+              [ viewSelectTextPreset model
+              , button [] [text "About"]
+              ]
+        ]
+
+type alias SampleText =
+    { id: String
+    , title: String
+    , content: String
+    }
+
+
+viewSelectTextPreset: Model -> Html Msg
+viewSelectTextPreset  model =
+    case model of
+        Success res ->
+          select [ class "select select-md rounded-none" ]
+              (option [ hidden True ] [text "Sample Text"]
+              :: (List.map createOption res.data))
+        Loading -> div [] []
+
+
+createOption: SampleText -> Html Msg
+createOption model =
+    option [] [text model.title]
+
+
+getSampleText : Cmd Msg
+getSampleText =
+    Http.get
+        { url = Env.hzm_api_url ++ "/texts"
+        , expect = Http.expectJson GotData (dataDecoder textDecoder) }
+
+
+type alias Data a = {data: List a}
+
+dataDecoder : Decoder a -> Decoder (Data a)
+dataDecoder decoder =
+    map Data
+        (field "data" (list decoder))
+
+
+textDecoder: Decoder SampleText
+textDecoder =
+    map3 SampleText
+        (field "id" string)
+        (field "title" string)
+        (field "text" string)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
+import fs from 'fs'
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite';
 import elmPlugin from 'vite-plugin-elm';
 
+dotenv.config({path: `.env.${process.env.APP_ENV ?? 'dev'}`})
+
+const createElmFile = (moduleName: string, vars: {[key: string]: string}) => {
+    // TODO: make it typed
+    const preamble = `module ${moduleName} exposing (..)`
+    const sortedVars = Object.entries(vars)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .map(([k, v]) => {
+            const varName = k.toLowerCase()
+            return [
+                `${varName}: String`,
+                `${varName} = "${v}"`,
+            ].join("\n")
+        })
+        .join("\n\n");
+
+
+    const target = `src/${moduleName}.elm`;
+    try {
+        fs.writeFileSync(target, [preamble, sortedVars].join("\n\n"))
+        console.log(`${target} file created.`)
+    } catch (err) {
+        console.error(`Failed to generate ${target} file from environment`)
+        throw err
+    }
+}
+
+const generateElmEnvPlugin = (options: {prefix: string}) => ({
+    name: "generate-elm-env",
+    buildStart(opt){
+        const prefix = options.prefix ?? "HZM_"
+        const filtered = Object.entries(process.env).filter(([k, v]) => k.startsWith(prefix))
+        const envs = Object.fromEntries(filtered);
+        createElmFile("Env", envs);
+    }
+})
+
 export default defineConfig({
-  plugins: [elmPlugin.default()],
+  plugins: [
+      generateElmEnvPlugin({prefix: "HZM_"}),
+      elmPlugin.default()
+  ],
 });
+
 
